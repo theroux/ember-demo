2b26b46f2ad527fa07e2226623cb752ad3c463fe
Andrew Theroux <therouxdown@gmail.com>
2016-02-09 13:32:36 -0500

    Basic tests implemented

--- tests/acceptance/user-can-visit-messages-test.js
+++ tests/acceptance/user-can-visit-messages-test.js
@@ -3,10 +3,12 @@ import moduleForAcceptance from 'mariana/tests/helpers/module-for-acceptance';
 
 moduleForAcceptance('Acceptance | user can visit messages');
 
-test('visiting /messages', function(assert) {
+test('Visiting /messages confirming markup', function(assert) {
   visit('/messages');
 
   andThen(function() {
-    assert.equal(currentURL(), '/messages');
+    assert.equal(currentURL(), '/messages', '/messages route defined');
+    assert.equal(find('.welcome').text(), 'Welcome to Ember.js', 'Page heading is present');
+    assert.equal(find('.message').length, 4, 'Correct number of messages displayed');
   });
 });
